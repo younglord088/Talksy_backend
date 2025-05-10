b0554f228526851245501f92a81edec3f544fc04
Yash Panjwani <yashmpanjwani@gmail.com>
2025-05-10 16:39:12 +0530

    Enable secure cookie setting in login response

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -86,7 +86,7 @@ export async function login(req, res) {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true, // prevent XSS attacks,
       sameSite: "strict", // prevent CSRF attacks
-      // secure: process.env.NODE_ENV === "production",
+      secure: process.env.NODE_ENV === "production",
     });
 
     res.status(200).json({ success: true, user });
